106d817b7cf5c29f71081cc7a89123a6fff27c9a
Linus T. Vettiger <linus.vettiger@gibmit.ch>
2018-05-25 15:48:17 +0200

    Connected offer form to firebase

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NouisliderComponent } from 'ng2-nouislider';
+import {AngularFirestore} from 'angularfire2/firestore';
 
 @Component({
     selector: 'app-contact',
@@ -8,9 +8,66 @@ import { NouisliderComponent } from 'ng2-nouislider';
 })
 
 export class ContactComponent {
+    frozen = false;
+    defaultModel = {
+      firstname: '',
+      surname: '',
+      email: '',
+      description: '',
+      budget: 500,
+      length: 1
+    };
+    error = {
+      firstname: null as string,
+      surname: null as string,
+      email: null as string,
+      description: null as string,
+    };
+    model = { ...this.defaultModel };
 
-    budgetRange: number[] = [500];
-    dauerRange: number[] = [1];
+    constructor(private db: AngularFirestore) { }
 
     onChange(ev) { }
+
+    send() {
+      if (this.model.firstname.length === 0) {
+        this.error.firstname = 'Required';
+      } else {
+        this.error.firstname = null;
+      }
+      if (this.model.surname.length === 0) {
+        this.error.surname = 'Required';
+      } else {
+        this.error.surname = null;
+      }
+      if (this.model.email.length === 0) {
+        this.error.email = 'Required';
+      } else if (!this.model.email.match(/^\w+@\w+.\w+$/)) {
+        this.error.email = 'Needs to be a valid e-mail address';
+      } else {
+        this.error.email = null;
+      }
+      if (this.model.description.length === 0) {
+        this.error.description = 'Required';
+      } else {
+        this.error.description = null;
+      }
+      if (this.error.firstname === null &&
+        this.error.surname === null &&
+        this.error.email === null &&
+        this.error.description === null) {
+
+        console.log('Success');
+        this.frozen = true;
+        this.db.collection('offers').add(this.model).then(response => {
+          this.frozen = false;
+          this.model = {
+            ...this.defaultModel,
+            firstname: this.model.firstname,
+            surname: this.model.surname,
+            email: this.model.email,
+          };
+        });
+      }
+    }
 }
